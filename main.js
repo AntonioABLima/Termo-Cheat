ed9e77240049b3001fa9897eddda8c3caf7924e3
Antônio Alexandre Borges Lima <104531655+AntonioABLima@users.noreply.github.com>
2023-09-02 14:49:14 -0300

    Update main.js
    Add list for all existing 5 word in pt-br dictionary

--- main.js
+++ main.js
@@ -1,74 +1,96 @@
-let focusedDiv = null;
+const nome_lista = 'ListaCincoPalavras.txt';
+let lista = [];
 
+(async () => {
+    try {
+        const response = await fetch('ListaCincoPalavras.txt'); // Substitua 'seuarquivo.txt' pelo nome do seu arquivo.
+        const text = await response.text();
 
-function addClickListenerToDivs(row) {
-    row.forEach(div => {
-        div.addEventListener('click', clickHandler);
-    });
-}
+		lista = text.split(/[\r\n]+/);
 
-function clickHandler() {
-    if (focusedDiv) {
-        focusedDiv.classList.remove('edit');
+        console.log('Arquivo lido com sucesso!');
+
+    } catch (error) {
+        console.error('Erro ao ler o arquivo:', error);
     }
-    focusedDiv = this;
-    focusedDiv.classList.add('edit');
-}
+})().then(() => {
+    // console.log(lista); // Isso será executado após a conclusão do bloco assíncrono
 
-function removeClickListenerFromDivs(row) {
-    row.forEach(div => {
-        div.removeEventListener('click', clickHandler);
-    });
-}
+	let focusedDiv = null;
 
-const row1 = document.querySelectorAll('.letter.empty');
-addClickListenerToDivs(row1);
-
-document.addEventListener('keydown', (event) => {
-	if (focusedDiv && event.keyCode > 64 && event.keyCode < 91 ) {
-		focusedDiv.textContent = event.key;
-	}
-	else if(focusedDiv){
-		focusedDiv.textContent = "";
-	}
-});
-
-var n_row = 0;
-const addDivButton = document.getElementById('addDivButton');
-addDivButton.addEventListener('click', () => {
-	if(n_row < 5 ){
-		n_row = n_row + 1;
-		const row =  document.querySelectorAll('#r' + n_row +' .letter');
+	function addClickListenerToDivs(row) {
 		row.forEach(div => {
-			div.classList.add('empty');
+			div.addEventListener('click', clickHandler);
 		});
-		addClickListenerToDivs(row);
-	};
-});
-
-const rmvDivButton = document.getElementById('rmvDivButton');
-rmvDivButton.addEventListener('click', () => {
-	if(n_row > 0 ){
-		const row =  document.querySelectorAll('#r' + n_row +' .letter');
-		row.forEach(div => {
-			focusedDiv = null;
-			div.innerHTML = "";
-			div.classList.remove('empty');
-			div.classList.remove('edit');
-		});
-		n_row = n_row - 1;
-		removeClickListenerFromDivs(row);
-	};
-});
-
-const printDivsButton = document.getElementById('printDivsButton');
-printDivsButton.addEventListener('click', () => {
-	const rows =  document.querySelectorAll('.row' +' .letter.empty');
-
-	for (let i = 0; i < rows.length; i += 5) {
-		var palavra = ""
-		palavra = rows[i].innerHTML + rows[i+1].innerHTML + rows[i+2].innerHTML + rows[i+3].innerHTML + rows[i+4].innerHTML; 	
-		console.log(palavra);
 	}
-	
-})
+
+	function clickHandler() {
+		if (focusedDiv) {
+			focusedDiv.classList.remove('edit');
+		}
+		focusedDiv = this;
+		focusedDiv.classList.add('edit');
+	}
+
+	function removeClickListenerFromDivs(row) {
+		row.forEach(div => {
+			div.removeEventListener('click', clickHandler);
+		});
+	}
+
+	const row1 = document.querySelectorAll('.letter.empty');
+	addClickListenerToDivs(row1);
+
+	document.addEventListener('keydown', (event) => {
+		if (focusedDiv && event.keyCode > 64 && event.keyCode < 91 ) {
+			focusedDiv.textContent = event.key;
+		}
+		else if(focusedDiv){
+			focusedDiv.textContent = "";
+		}
+	});
+
+	var n_row = 0;
+	const addDivButton = document.getElementById('addDivButton');
+	addDivButton.addEventListener('click', () => {
+		if(n_row < 5 ){
+			n_row = n_row + 1;
+			const row =  document.querySelectorAll('#r' + n_row +' .letter');
+			row.forEach(div => {
+				div.classList.add('empty');
+			});
+			addClickListenerToDivs(row);
+		};
+	});
+
+	const rmvDivButton = document.getElementById('rmvDivButton');
+	rmvDivButton.addEventListener('click', () => {
+		if(n_row > 0 ){
+			const row =  document.querySelectorAll('#r' + n_row +' .letter');
+			row.forEach(div => {
+				focusedDiv = null;
+				div.innerHTML = "";
+				div.classList.remove('empty');
+				div.classList.remove('edit');
+			});
+			n_row = n_row - 1;
+			removeClickListenerFromDivs(row);
+		};
+	});
+
+
+	const printDivsButton = document.getElementById('printDivsButton');
+	printDivsButton.addEventListener('click', () => {
+		const rows =  document.querySelectorAll('.row' +' .letter.empty');
+
+		for (let i = 0; i < rows.length; i += 5) {
+			var palavra = ""
+			palavra = rows[i].innerHTML + rows[i+1].innerHTML + rows[i+2].innerHTML + rows[i+3].innerHTML + rows[i+4].innerHTML; 	
+			console.log(lista.includes(palavra));
+		}
+	})
+
+
+});
+
+
